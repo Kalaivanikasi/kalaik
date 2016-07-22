1e1b8b6f56711af9166817cec851ac8580ba2e9e
Kalaivani <kalaivani.kcse@ksrce.ac.in>
2016-07-22 15:14:32 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,20 @@
+class vowels
+{
+public static void main()
+{
+char(c)
+switch(c)
+char 'a':
+System.out.println("a is vowels");
+case 'e':
+System.out.println("e is vowels");
+case 'i':
+System.out.println("i is vowels");
+case 'o':
+System.out.println("o is vowels");
+case 'u':
+System.out.println("i is vowels");
+break;
+System.out.println("num is alaphet");
+}
+}
